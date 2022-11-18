830330716090eeb4338e875062c3fbce1b710a19
raul435 <a01742153@tec.mx>
2022-11-18 02:49:07 +0000

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int tam;
+    int num;
+    int XDFS;
+    int XBFS;
+    vector<vector<int> > matriz = {
+      {0,1,0,0,0},
+      {1,0,1,0,0},
+      {0,1,0,1,1},
+      {0,0,1,0,1},
+      {0,0,0,1,0}};
+
+    
+    return 0;
+}
+  
